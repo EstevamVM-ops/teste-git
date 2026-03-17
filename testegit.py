4c632ba48626707f6c516a8ed42ddf8fcc06bca7
Esteva V.M. <estevamvigilatomatsuyama@gmail.com>
2026-03-17 07:43:19 -0300

    Mensagem

--- testegit.py
+++ testegit.py
@@ -1,5 +1,23 @@
+
+
 try:
  n = int(input('Digite um numero:'))
  print( n + 2)
+ 
+ 
+ 
+ 
+ 
 except :
  print ('Insersão errada')
+n = int(input('Digite um numero:'))
+
+
+
+for i in range(n):
+  
+  i*=i
+  
+ 
+  
+print( i )
