8a90dbe9c0b121a153344288ca13b365097b8ccf
Esteva V.M. <estevamvigilatomatsuyama@gmail.com>
2026-03-17 08:09:14 -0300

    add fatorial

--- testegit.py
+++ testegit.py
@@ -1,23 +1,33 @@
-
-
+def fatorial (n:int)->int:
+        '''
+        algoritimo interativo para fatorial                   
+        input:                                                 re   
+        n:int - Um valor inteiro qualquer > 0
+        output
+        result - Um valor inteiro qualquer > 0
+        '''
+        a=1
+        i=1
+        while i <= 5:
+                a*=i
+                i+=1
+        return a        
+        print( a)
+        '''usando for
+        res=1
+        for i in range(1,n+1):
+            res*= i
+        return res
+        print(res) 
+        ;)
+            
+        '''
 try:
  n = int(input('Digite um numero:'))
  print( n + 2)
- 
- 
- 
- 
- 
+ print(fatorial(n))
 except :
  print ('Insersão errada')
 n = int(input('Digite um numero:'))
 
 
-
-for i in range(n):
-  
-  i*=i
-  
- 
-  
-print( i )
